2ee2a9d61844dfdfe27b122b7b7dfe3fa6f69786
HidekiMichi <105813974+HidekiMichi@users.noreply.github.com>
2024-01-31 14:23:03 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
 var dateDiffInDays = function (date1, date2) {
   //   write your code here
+	let dateOne=date1.split('-');
+	let dateTwo=date2.split('-');
+	return dateTwo[2]-dateOne[2];
 };
 
 // Do not change the code below.
